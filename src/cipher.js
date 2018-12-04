1aec9659af08f5c43e0ff8f8bb80103319320847
LoreMiranda <lorena.miranda.lagos@gmail.com>
2018-12-04 11:54:16 -0300

    cipher funcional

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,34 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (fraseInicial, key) => {
+    let texto="";
+    for (let i=0; i< fraseInicial.length;i++){
+      let sdotexto=fraseInicial.charCodeAt(i);
+        if (sdotexto >=65 && sdotexto<=90){
+        let code=(sdotexto-65+key)%26+65;
+        texto+=String.fromCharCode(code);
+      }
+      
+    }
+
+    return texto;
+
+    
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (fraseInicial, key) => {
+    let texto="";
+    for (let i=0; i< fraseInicial.length;i++){
+      let sdotexto=fraseInicial.charCodeAt(i);
+        if (sdotexto >=65 && sdotexto<=90){
+        let code=(sdotexto-65-key)%26+65;
+        if (code<65){
+          code+=26;
+        }
+        texto+=String.fromCharCode(code);
+      }
+      
+    }
+
+    return texto;
   }
+
 };
