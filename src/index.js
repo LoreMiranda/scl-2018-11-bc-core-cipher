9ad7ee7f0052684477a564f6d94a44917d74d012
LoreMiranda <lorena.miranda.lagos@gmail.com>
2018-12-07 01:11:53 -0300

    proyecto cipher con 7 test funcionando

--- src/index.js
+++ src/index.js
@@ -1,61 +1,20 @@
-//window.onload = {
-//declaracion de variables y arreglos vacios
 
-/*let message;
-let string = [];
-let code = [];
-let n;
-let j;
-let newCode = [];
-
-function encodeText() //funcion que codifica el texto
-{
- message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
-  n = document.getElementById("key").value; //n guarda el numero que se recorre el desencriptado
-  for(j = 0; j < message.length; j++){ //for que recorre el message
-  	  string.push((message.charCodeAt(j))-65+n%26+65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
-      code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en alfanumerico, y se guarda en code
-      }
-      newCode = code.join("");
-      document.getElementById("cifrado").innerHTML = (newCode.toUpperCase()); //coloca el mensaje encriptado en un recuadro abajo
-  
- /* console.log(string); muestra codigo ASCII
- console.log(code); muestra mensaje encriptado */
-      /*string = []; //vacia la cadena
-      code = []; //vacia la cadena
-}
-/*function decodeText() //funcion que decodifica el texto
-{
-  message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
-  n = document.getElementById("key").value; //n guarda el numero que se recorre el desencriptado
-  for(j = 0; j < message.length; j++){ //for que recorre el message
-  	  string.push((message.charCodeAt(j))+65-n%26-65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
-  	  code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en letras, y se guarda en code
-      }
-      newCode = code.join("");
-      document.getElementById("descifrado").innerHTML = (newCode.toUpperCase()); //coloca el mensaje desencriptado en un recuadro abajo
+document.getElementById("btncode").addEventListener("click", () => {
+     let encryptText = (document.getElementById("fraseInicial").value).toUpperCase();
+     let offset = document.getElementById("key").value;
+     offset = parseInt(offset);
+     document.getElementById("descifrado").innerHTML = (cipher.encode(encryptText, offset));
  
- /* console.log(string); muestra codigo ASCII
- console.log(code); muestra mensaje desencriptado */
-     /* string = []; //vacia la cadena
-      code = []; //vacia la cadena
-}*/
-
-
-
-function encodeText(){
-     let encryptText = document.getElementById("fraseInicial").value;
+ })
+ 
+ 
+ 
+ 
+ document.getElementById("btnuncode").addEventListener("click", () => {
+ 
+     let encryptText = (document.getElementById("fraseInicial").value).toUpperCase();
      let offset = document.getElementById("key").value;
-     offset=parseInt(offset);
-     document.getElementById("descifrado").innerHTML=cipher.encode(encryptText, offset);
-
-
-
-}
-function uncodeText(){
-
-     let encryptText = document.getElementById("fraseInicial").value;
-     let offset = document.getElementById("key").value;
-     offset=parseInt(offset);
-     document.getElementById("descifrado").innerHTML=cipher.decode(encryptText, offset);
-}
+     offset = parseInt(offset);
+     document.getElementById("descifrado").innerHTML = cipher.decode(encryptText, offset);
+ 
+ })
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,25 +1,41 @@
 describe('cipher', () => {
 
   it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
+      assert.equal(typeof cipher, 'object');
   });
 
   describe('cipher.encode', () => {
 
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
-    });
+      it('debería ser una función', () => {
+          assert.equal(typeof cipher.encode, 'function');
+      });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+      it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
+
+          assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 33), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+      });
+      it('deberia retornar " " para " " con offset 1', () => {
+          assert.equal(cipher.encode(" ", 1), " ");
+      });
+
+
   });
 
   describe('cipher.decode', () => {
 
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
-    });
+      it('debería ser una función', () => {
+          assert.equal(typeof cipher.decode, 'function');
+      });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
-  });
+      it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
 
-});
+          assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+      });
+      it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "abcdefghijklmnopqrstuvwxyz" con offest 1', () => {
+
+          assert.equal(cipher.decode("abcdefghijklmnopqrstuvwxyz", 1), "abcdefghijklmnopqrstuvwxyz");
+
+      });
+  })
+})
