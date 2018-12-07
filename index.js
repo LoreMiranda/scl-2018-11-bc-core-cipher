98178d2ee23f7e4b743a69f930de8dd422a06524
LoreMiranda <lorena.miranda.lagos@gmail.com>
2018-12-07 02:14:17 -0300

    se agregan comentarios

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/* le decimos al botón "codificar" que al hacer click guarde en la variable "encrypText" lo que
+el usuario ingresa en el input "fraseInicial", y que antes de cualquier cosa, lo devuelva en mayúsculas.
+Luego llamamos a la variable offset para que almacene el desplazamiento que escoje el usuario,
+para finalmente llamar al input de "descifrado" nos devuelva el resultado de la función
+que le indicamos en el cipher.js, en el bloque de encode */
 
 document.getElementById("btncode").addEventListener("click", () => {
      let encryptText = (document.getElementById("fraseInicial").value).toUpperCase();
@@ -6,9 +11,12 @@ document.getElementById("btncode").addEventListener("click", () => {
      document.getElementById("descifrado").innerHTML = (cipher.encode(encryptText, offset));
  
  })
- 
- 
- 
+  
+ /* le decimos al botón "decodificar" que al hacer click guarde en la variable "encrypText" lo que
+el usuario ingresa en el input "fraseInicial", y que antes de cualquier cosa, lo devuelva en mayúsculas.
+Luego llamamos a la variable offset para que almacene el desplazamiento que escoje el usuario,
+para finalmente llamar al input de "descifrado" nos devuelva el resultado de la función
+que le indicamos en el cipher.js, en el bloque de decode */
  
  document.getElementById("btnuncode").addEventListener("click", () => {
  
